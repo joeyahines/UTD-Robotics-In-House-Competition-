544ca309d66c17a82bf63d889257cc1ac2add185
Joey Hines <joey.ahines@gmail.com>
2017-10-12 15:30:12 -0500

    Implemented UTDRWirelessComms Library

---
+++ Libraries/UTDRWirelessComms/Examples/ParseSerialPacket/ParseSerialPacket.ino
@@ -0,0 +1,41 @@
+/*  ParseSerialPacketExample
+ *  Written By: Joey Hines (joey.ahines@gmail.com)
+ *  
+ *  Parses a packet from serial and prints its part to the user
+ */
+
+#include <UTDRWirelessComms.h>
+
+//Create new UTDRWirelessComms named input
+UTDRWirelessComms input = new UTDRWirelessComms();
+ 
+void setup() {
+  //Setup Serial
+  Serial.begin(9600);
+  Serial.println("Starting...");
+}
+
+void loop() {
+  //Wait for serial
+  if (Serial.available()) {
+    //New int array for storing packet data
+    int * packet = new int[3];
+
+    //Get the command packet
+    input.getCommandPacket(packet);
+
+    //Echo packet to user
+    Serial.println("Packet: ");
+    for (int i = 0; i < 3; i++) {
+      Serial.print(packet[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+
+    //Clear packet memory
+    delete packet;
+  }
+
+  delay(20);
+
+}
---
+++ Libraries/UTDRWirelessComms/readme.txt
@@ -0,0 +1,12 @@
+UT Dallas Robotics Wireless Communication Library v1
+Written by Joey Hines (joey.ahines@gmail.com)
+
+This library allows for parsing packets from serial in the style of 
+"~{type}{id}{data}\LF". It is to be used with a ESP8266 and the UTD
+Robotics wireless controller.
+
+To Install:
+1. Download the UTDRWirelessComms folder
+2. Copy folder to the Arduino library folder (Usually located under C:\Program Files\Arduino\Library)
+3. Include it in the Arduino Sketch you need it for (#include <UTDRWirelessComms.h>)
+4. Compile and Upload Code to Arduino
